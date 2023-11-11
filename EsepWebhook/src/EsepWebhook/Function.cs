5fcff9b85857ced98f7d634ef088948be5778690
Carlos Avila <48300738+RetroSpaceMan123@users.noreply.github.com>
2023-11-10 21:47:51 -0500

    Create Lambda Function for GitHub Issues

---
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -0,0 +1,40 @@
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace EsepWebhook;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(string input, ILambdaContext context)
+    {
+        // This is using the sample code from here: https://github.com/jcutrono/esep-webhooks/blob/main/EsepWebhook/src/EsepWebhook/Function.cs
+        dynamic json = JsonSerializer.Deserialize<dynamic>(input);
+        
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        {
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+        };
+
+        var response = client.Send(webRequest);
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+        
+        return reader.ReadToEnd();
+    }
+}
